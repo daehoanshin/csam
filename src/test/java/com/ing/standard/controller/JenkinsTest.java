edab22cb1710da5181a68fc8b178368895d96b2e
xbb123 <xbb123@ubudev.com>
2017-11-27 22:58:59 +0900

    처음 commit

---
+++ src/test/java/com/ing/standard/controller/JenkinsTest.java
@@ -0,0 +1,11 @@
+package com.ing.standard.controller;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class JenkinsTest {
+	@Test
+	public void jenkinsTest() {
+		assertEquals("1", 1);
+	}
+}
